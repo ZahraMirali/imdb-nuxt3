5b0fa154615f73a6137c4a239307a5901b8ebc39
Zahra Mirali <z.mirali1997@gmail.com>
2023-08-23 15:29:57 +0200

    Routing

--- app.vue
+++ app.vue
@@ -1,28 +1,3 @@
 <template>
-  <button @click="toggle">Show Welcome</button>
-  <component :is="MyComponent1" />
-
-  <hr />
-
-  <LazyHomeWelcome v-if="MyComponent2 === 'welcome'"/>
-  <Counter v-else />
-
-  <hr />
-
-  <a href="#" @click="view('welcome')">Welcome </a>
-  <a href="#" @click="view('counter')">Counter</a>
+  <NuxtPage />
 </template>
-
-<script setup>
-  const MyComponent1 = shallowRef(resolveComponent("Counter"));
-  const MyComponent2 = ref("counter");
-
-  function toggle(){
-    MyComponent1.value = resolveComponent("HomeWelcome")
-    MyComponent2.value = 'welcome';
-  }
-
-  function view(name){
-    MyComponent2.value = name;
-  }
-</script>
