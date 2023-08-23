eb2e50bfa1c8f058529462f468308e864cf9c3b0
Zahra Mirali <z.mirali1997@gmail.com>
2023-08-23 16:53:56 +0200

    Dynamic Routes

---
+++ pages/iphone/[version].vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>iPhone {{ $route.params.version }} :) {{ version }}</h1>
+</template>
+
+
+<script setup>
+    const route = useRoute();
+
+    const version = computed(()=> {
+        return route.params.version
+    })
+</script>
