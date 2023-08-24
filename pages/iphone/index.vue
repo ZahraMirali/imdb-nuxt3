c2778c2d9ab86e9c4e078ec22c2f5d5d371fc85c
Zahra Mirali <z.mirali1997@gmail.com>
2023-08-24 19:32:37 +0200

    Server Routes

--- pages/iphone/index.vue
+++ pages/iphone/index.vue
@@ -1,10 +1,15 @@
 <template>
     <h1>iPhones page</h1>
-    <img width="200" src="/images/iphone14.webp" />
+    <NuxtLink v-for="iphone in iphones" :key="iphone" :to="`/iphone/${iphone}`">
+        <img width="200" src="/images/iphone14.webp" />
+    </NuxtLink>
 </template>
 
 <script setup>
-    useHead({
-        title: 'My iPhones page'
-    })
+useHead({
+    title: "My iPhones page",
+});
+
+const { data } = await useFetch("/api/iphone");
+const iphones = data;
 </script>
---
+++ server/api/iphone.ts
@@ -0,0 +1,3 @@
+export default defineEventHandler(() => {
+  return { data: ["13", "14"] };
+});
